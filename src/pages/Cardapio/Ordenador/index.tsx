7ae969ddaa2e1fc577c49cd8c5f137f3baff545f
Rafael <rb2335@gmail.com>
2022-08-18 18:45:52 -0300

    Versao com routes

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -13,34 +13,34 @@ interface IOrdenador {
   }
 
 export default function Ordenador({ ordenador, setOrdenador}: IOrdenador) {
-    const [aberto, setAberto] = useState(false);
-    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
-    return (
-        <button 
-            className={classNames({
-                [styles.ordenador]: true,
-                [styles['ordenador--ativo']]: ordenador !== ''
-            })} 
-            onClick={() => setAberto(!aberto)}
-            onBlur={() => setAberto(false)} //clique fora
-        >
-            <span>{nomeOrdenador || 'Ordenar Por'}</span>
-            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
-            <div className={classNames({
-                    [styles.ordenador__options]: true,
-                    [styles['ordenador__options--ativo']]: aberto
-                })}
-            >
-                {opcoes.map(opcao => (
-                    <div 
-                        className={styles.ordenador__option} 
-                        key={opcao.value}
-                        onClick={() => setOrdenador(opcao.value as OpcoesOrdenador)}
-                    >
-                        {opcao.nome}
-                    </div>
-                ))}
-            </div>
-        </button>
-    );
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+  return (
+    <button 
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles['ordenador--ativo']]: ordenador !== ''
+      })} 
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)} //clique fora
+    >
+      <span>{nomeOrdenador || 'Ordenar Por'}</span>
+      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles['ordenador__options--ativo']]: aberto
+      })}
+      >
+        {opcoes.map(opcao => (
+          <div 
+            className={styles.ordenador__option} 
+            key={opcao.value}
+            onClick={() => setOrdenador(opcao.value as OpcoesOrdenador)}
+          >
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
 }
